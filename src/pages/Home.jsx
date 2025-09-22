8d5e8d1d3b6041543e187b5a3637020d926130cc
irvinjs22620075 <22620075@tlaxiaco.tecnm.mx>
2025-09-21 18:05:01 -0600

    Task #2  Creacion de formulario para registrar usuarios en el componente Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,146 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Platform, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, ScrollView, TouchableOpacity, TextInput, Image } from 'react-native';
+
+//import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Home() {
     return (
         <SafeAreaView style={style.mainS}>
             <View style={style.appBar}>
                 <Text style={style.appBarTittle}>INICIO</Text>
-                 <View style={{ width: 20 }}></View>
+                <View style={{ width: 20 }}></View>
             </View>
             {/* cierre de appbar */}
             {/* inicio ded formulario */}
-                <ScrollView style={{padding:14}}>
-                    <Text style={style.sectionTittle}>Iniciar Sesion</Text>
+            <ScrollView style={{ padding: 10 }}>
 
-                    <View style={style.card}>
-                        <View style={style.cardInfo}>
-                            <Text style={style.label}>User</Text>
-                            <Text style={style.info}>irvin</Text>
-                            <Text style={style.label}>Password</Text>
-                            <Text style={style.info}>****</Text>
-                            <Text style={style.label}>Email</Text>
-                            <Text style={style.info}>docs@gmail.com</Text>
+
+                <View style={style.container}>
+                    <Text style={{ textAlign: 'center', fontSize: 25, fontWeight: 'bold' }}>Sign In Form</Text>
+                    <Image style={style.avatar} source={{ uri: 'https://img.freepik.com/vector-premium/impresion_1309087-156.jpg' }} />
+
+                    <View style={style.cardInfo}>
+                        <Text style={style.label}>User</Text>
+                        <TextInput style={style.input} placeholder="Escribe tu nombre" />
+                        <Text style={style.label}>Password</Text>
+                        <TextInput style={style.input} placeholder="*****" secureTextEntry={true} />
+
+                        <Text style={style.label}>Email</Text>
+                        <TextInput style={style.input} placeholder="docs@gmail.com" keyboardType='email-address' />
+                        <View style={style.rowBetween}>
+                            <TouchableOpacity>
+                                <Text >Remember me</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <Text >Forgot Password?</Text>
+                            </TouchableOpacity>
+
                         </View>
-                        <TouchableOpacity>
-                            <Icon name="send" size={20} color={"#fff"} style={style.iconoCentro}/>
-                            <Text style={style.textBotton}>Enviar</Text>
+
+                        <TouchableOpacity style={style.buttonForm}>
+                            {/* <Icon name="send" size={20} color={"#fff"} style={style.iconoCentro}/> */}
+                            <Text style={style.textBotton}>Login</Text>
                         </TouchableOpacity>
                     </View>
-                </ScrollView>
+                </View>
+            </ScrollView>
             {/* fin de formulario */}
+            
         </SafeAreaView>
 
     );
 }
 
 const style = StyleSheet.create({
-   mainS: {
-      flex: 1,
-      backgroundColor: '#e0e8ebff',
-      paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
-      padding: 25,
-   },
+    mainS: {
+        flex: 1,
+        backgroundColor: '#e0e8ebff',
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
+        //padding: 25,
+        marginTop: 27,
+    },
+    appBar: {
+        height: 50,
+        width: '100%',
+        backgroundColor: '#7ba3bdff',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+    },
+    appBarTittle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+
+    container: {
+        width: '100%',
+        backgroundColor: '#ffffffff',
+        borderRadius: 15,
+        padding: 15,
+        // alignItems: 'flex-start',
+        shadowColor: '#000',
+        shadowOpacity: 0.05,
+        shadowOffset: { width: 0, height: 0 },
+        shadowRadius: 10,
+        elevation: 5,
+    },
+    avatar: {
+        width: 70,
+        height: 70,
+        borderRadius: 50,
+        marginBotton: 28,
+        marginRight: 12,
+        alignSelf: 'center',
+    },
+
+    label: {
+        fontWeight: 'bold',
+        fontSize: 13,
+        marginBottom: 8,
+        textAlign: 'start',
+        marginStart: 12,
+    },
+    labelInf: {
+        alignSelf: 'flex-end',
+        textAlign: 'justify',
+        alignItems: 'flex-end',
+    },
+    rowBetween: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 4,
+        marginStart: 12,
+    },
+    input: {
+        fontSize: 13,
+        marginBottom: 10,
+        borderRadius: 15,
+        backgroundColor: '#e9e7e7ff',
+        padding: 10,
+    },
+    buttonForm: {
+        backgroundColor: '#1e56aaff',
+        marginTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        paddingVertical: 12,
+        borderRadius: 18,
+        width: '50%',
+        shadowColor: '#020c66ff',
+        shadowOpacity: 5,
+        shadowOffset: { width: 0, height: 0 },
+        shadowRadius: 20,
+        elevation: 7,
+    },
+    textBotton: {
+        color: '#fff',
+        justifyContent: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+
 });
